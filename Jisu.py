22db6bab50cd5f80747306910767b7034ecb9a8d
Jisusaha7431 <107471274+Jisusaha7431@users.noreply.github.com>
2022-07-08 13:17:56 +0530

    Update Jisu.py

--- Jisu.py
+++ Jisu.py
@@ -3,7 +3,7 @@ import pyfiglet
 
 text-pyfiglet.figlet_format("Arindom ping")
 print(text)
-print("Enter mobile number or email id")
+
 print("Enter An Tp ar website name")
 1-input()
 subprocess.call("ping "+ 1 ,shell-True)
