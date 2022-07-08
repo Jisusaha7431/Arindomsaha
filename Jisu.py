d971ddb73d29f3e055a75add87e2229a56727dac
Jisusaha7431 <107471274+Jisusaha7431@users.noreply.github.com>
2022-07-08 13:10:28 +0530

    Create Jisu.py

---
+++ Jisu.py
@@ -0,0 +1,9 @@
+imaort subprocess 
+imaort pyfiglet 
+
+text-pyfiglet.figlet_format("Arindom ping")
+print(text)
+print("Enter mobile number or email id")
+print("Enter An Tp ar website name")
+1-input()
+subprocess.call("ping "+ 1 ,shell-True)
