71a21ff1e4ad46fd142e8f4903b6aca56a471213
Jisusaha7431 <107471274+Jisusaha7431@users.noreply.github.com>
2022-07-12 15:50:13 +0530

    Create Jsaha.py

---
+++ Jsaha.py
@@ -0,0 +1,23 @@
+Import phonenumbers
+
+from phonenumbers import timezone, geocoder, carrier
+
+
+
+number = input("Enter Your Phone Number: ")
+
+phone phonenumbers.parse(number)
+
+time = timezone.time zones_for_number (phone)
+
+sin details = carrier.name for number (phone, "en")
+
+register geocoder.description_for_number (phone "en")
+
+
+print (number)
+print (phone)
+print(time)
+print (sin details)
+print (register)
+
